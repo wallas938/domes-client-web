9845c80fe9a16c8abfb17250d577eea9a569970b
Wallas9_3 <drame.sissako001@gmail.com>
2023-02-25 00:44:48 +0100

    home page for mobile OP

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import {HttpClientModule} from "@angular/common/http";
+import { DomesButtonComponent } from './components/domes-button/domes-button.component';
 
 @NgModule({
   declarations: [
+
+    DomesButtonComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
   exports: [
-    HttpClientModule
+    HttpClientModule,
+    DomesButtonComponent
   ],
   providers: [],
 })
