ae8ca4931e055da7f39aa933e17846e594b53571
Wallas9_3 <drame.sissako001@gmail.com>
2023-03-10 01:20:49 +0100

    ngrx store installed

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,13 +1,15 @@
-import {Component} from '@angular/core';
+import {Component} from "@angular/core";
 import {Article} from "../../models/animal";
+import {SECTION_NAMES} from "src/app/constants/index";
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent {
 
+  SECTION_NAMES=SECTION_NAMES;
   articles: Article[] = [
     {
       id: '0b016d38-c176-4065-8f3e-d754a3c3876d',
@@ -30,4 +32,12 @@ export class CartComponent {
   ]
   months: string[] = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
   years: string[] = ["2032", "2031", "2030", "2029", "2028", "2027", "2026", "2025", "2024", "2023", "2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012"];
+
+
+  constructor() {
+  }
+
+  toggleSection(section: string) {
+    console.log(section)
+  }
 }
---
+++ src/store/actions/layout.actions.ts
@@ -0,0 +1,16 @@
+import { createAction, props } from "@ngrx/store";
+
+/* FILTER MODAL IDENTIFIERS */
+
+// const USER_IDENTITY_MODAL_OPENED = '[UI] USER_IDENTITY_MODAL_OPENED';
+// const USER_IDENTITY_MODAL_CLOSED = '[UI] USER_IDENTITY_MODAL_CLOSED';
+
+/* MOBILE MENU ACTIONS */
+
+// const UserIdentityModalOpened = createAction(USER_IDENTITY_MODAL_OPENED);
+// const UserIdentityModalClosed = createAction(USER_IDENTITY_MODAL_CLOSED);
+
+export const LayoutActions = {
+  // UserIdentityModalOpened,
+  // UserIdentityModalClosed,
+};
