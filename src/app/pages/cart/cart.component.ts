0388f37bfe97860bf32b4e230ffa1f2d567d4710
Wallas9_3 <drame.sissako001@gmail.com>
2023-03-13 00:27:52 +0100

    sections dropdown animations are OP

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,17 @@
 import {Component} from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes
+} from '@angular/animations';
+
+/*
+* Custom imports
+* */
+
 import {Article} from "../../models/animal";
 import {SECTION_NAMES} from "src/app/constants/index";
 
@@ -6,10 +19,42 @@ import {SECTION_NAMES} from "src/app/constants/index";
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
+  animations: [
+    trigger('openClose', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-100px)' }),
+        animate('.3s', style({opacity: 1, transform: 'translateY(0px)'})),
+      ]),
+      transition(':leave', [
+        animate('.3s', style({opacity: 0, transform: 'translateY(-200px)'}))
+      ])
+    ]),
+    trigger('rotate180deg', [
+      // ...
+      state('up', style({
+        transform: 'rotateZ(0deg)'
+      })),
+      state('down', style({
+        transform: 'rotateZ(180deg)'
+      })),
+      transition('up => down', [
+        animate('.3s')
+      ]),
+      transition('down => up', [
+        animate('.3s')
+      ]),
+    ]),
+  ]
 })
 export class CartComponent {
 
-  SECTION_NAMES=SECTION_NAMES;
+  sectionStates = {
+    resume: true,
+    shippingInfo: true,
+    payment: true
+  }
+
+  SECTION_NAMES = SECTION_NAMES;
   articles: Article[] = [
     {
       id: '0b016d38-c176-4065-8f3e-d754a3c3876d',
@@ -38,6 +83,19 @@ export class CartComponent {
   }
 
   toggleSection(section: string) {
-    console.log(section)
+    switch (section) {
+      case SECTION_NAMES.RESUME: {
+        this.sectionStates.resume = !this.sectionStates.resume;
+        break;
+      }
+      case SECTION_NAMES.SHIPPING_INFO: {
+        this.sectionStates.shippingInfo = !this.sectionStates.shippingInfo;
+        break;
+      }
+      case SECTION_NAMES.PAYMENT: {
+        this.sectionStates.payment = !this.sectionStates.payment;
+        break;
+      }
+    }
   }
 }
